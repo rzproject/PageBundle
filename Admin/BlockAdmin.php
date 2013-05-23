99beb6af25d87b5fae02d0fde57bde32edbcfbe2
Mell Zamora <mellzamora@outlook.com>
2013-05-23 09:17:59 +0800

    added license

--- Admin/BlockAdmin.php
+++ Admin/BlockAdmin.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RzPageBundle package.
+ *
+ * (c) mell m. zamora <mell@rzproject.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace  Rz\PageBundle\Admin;
 
 use Sonata\PageBundle\Admin\BlockAdmin as BaseBlockAdmin;
