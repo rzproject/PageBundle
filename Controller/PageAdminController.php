b332244a4991e8c2a3080e4f2907271d736e48d1
Mell Zamora <mellzamora@outlook.com>
2013-11-15 10:06:41 +0800

    fixed admin layout

---
+++ Controller/PageAdminController.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Rz\PageBundle\Controller;
+
+use Sonata\PageBundle\Controller\PageAdminController as Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Sonata\AdminBundle\Exception\ModelManagerException;
+use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Sonata\AdminBundle\Admin\BaseFieldDescription;
+
+
+class PageAdminController extends Controller
+{
+    /**
+     * @param mixed $query
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function batchActionSnapshot($query)
+    {
+        if (!$this->get('security.context')->isGranted('ROLE_SONATA_PAGE_ADMIN_PAGE_EDIT')) {
+            throw new AccessDeniedException();
+        }
+
+        foreach ($query->execute() as $page) {
+            $this->get('sonata.notification.backend')
+                ->createAndPublish('sonata.page.create_snapshot', array(
+                    'pageId' => $page->getId(),
+                ));
+        }
+
+        return new RedirectResponse($this->admin->generateUrl('list', $this->admin->getFilterParameters()));
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function createAction()
+    {
+        if (false === $this->admin->isGranted('CREATE')) {
+            throw new AccessDeniedException();
+        }
+
+        if ($this->getRequest()->getMethod() == 'GET' && !$this->getRequest()->get('siteId')) {
+            $sites = $this->get('sonata.page.manager.site')->findBy();
+
+            if (count($sites) == 1) {
+                return $this->redirect($this->admin->generateUrl('create', array(
+                    'siteId' => $sites[0]->getId(),
+                    'uniqid' => $this->admin->getUniqid()
+                )));
+            }
+
+            try {
+                $current = $this->get('sonata.page.site.selector')->retrieve();
+            } catch (\RuntimeException $e) {
+                $current = false;
+            }
+
+            return $this->render('SonataPageBundle:PageAdmin:select_site.html.twig', array(
+                'sites'   => $sites,
+                'current' => $current,
+            ));
+        }
+
+        return parent::createAction();
+    }
+    /**
+     * return the Response object associated to the edit action
+     *
+     *
+     * @param mixed $id
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     *
+     * @return Response
+     */
+       /**
+     * return the Response object associated to the edit action
+     *
+     *
+     * @param mixed $id
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     *
+     * @return Response
+     */
+    public function editAction($id = null)
+    {
+        // the key used to lookup the template
+        $templateKey = 'edit';
+
+        $id = $this->get('request')->get($this->admin->getIdParameter());
+
+        $object = $this->admin->getObject($id);
+
+        if (!$object) {
+            throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
+        }
+
+        if (false === $this->admin->isGranted('EDIT', $object)) {
+            throw new AccessDeniedException();
+        }
+
+        $this->admin->setSubject($object);
+
+        /** @var $form \Symfony\Component\Form\Form */
+        $form = $this->admin->getForm();
+        $form->setData($object);
+
+        if ($this->getRestMethod() == 'POST') {
+
+            $form->handleRequest($this->get('request'));
+
+            $isFormValid = $form->isValid();
+
+            // persist if the form was valid and if in preview mode the preview was approved
+            if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
+                $this->admin->update($object);
+                $this->addFlash('sonata_flash_success', 'flash_edit_success');
+
+                if ($this->isXmlHttpRequest()) {
+                    return $this->renderJson(array(
+                        'result'    => 'ok',
+                        'objectId'  => $this->admin->getNormalizedIdentifier($object)
+                    ));
+                }
+
+                // redirect to edit mode
+                return $this->redirectTo($object);
+            }
+
+            // show an error message if the form failed validation
+            if (!$isFormValid) {
+                if (!$this->isXmlHttpRequest()) {
+                    $this->addFlash('sonata_flash_error', 'flash_edit_error');
+                }
+            } elseif ($this->isPreviewRequested()) {
+                // enable the preview template if the form was valid and preview was requested
+                $templateKey = 'preview';
+            }
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+        return $this->render($this->admin->getTemplate($templateKey), array(
+            'action' => 'edit',
+            'form'   => $view,
+            'object' => $object,
+        ));
+    }
+
+}
