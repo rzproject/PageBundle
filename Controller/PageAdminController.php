fa88a9930804f03070031feab90384c780f12c87
Mell Zamora <mellzamora@outlook.com>
2014-01-08 17:26:50 +0800

    updated page admin controller

--- Controller/PageAdminController.php
+++ Controller/PageAdminController.php
@@ -16,7 +16,65 @@ use Sonata\AdminBundle\Admin\BaseFieldDescription;
 
 class PageAdminController extends Controller
 {
+    /**
+     * @param mixed $query
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function batchActionSnapshot($query)
+    {
+        if (!$this->get('security.context')->isGranted('ROLE_SONATA_PAGE_ADMIN_PAGE_EDIT')) {
+            throw new AccessDeniedException();
+        }
 
+        foreach ($query->execute() as $page) {
+            $this->get('sonata.notification.backend')
+                ->createAndPublish('sonata.page.create_snapshot', array(
+                    'pageId' => $page->getId(),
+                ));
+        }
+
+        return new RedirectResponse($this->admin->generateUrl('list', $this->admin->getFilterParameters()));
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function createAction()
+    {
+        if (false === $this->admin->isGranted('CREATE')) {
+            throw new AccessDeniedException();
+        }
+
+        if ($this->getRequest()->getMethod() == 'GET' && !$this->getRequest()->get('siteId')) {
+            $sites = $this->get('sonata.page.manager.site')->findBy();
+
+            if (count($sites) == 1) {
+
+                return $this->redirect($this->admin->generateUrl('create', array(
+                    'siteId' => $sites[0]->getId(),
+                    'uniqid' => $this->admin->getUniqid()
+                )));
+            }
+
+            try {
+                $current = $this->get('sonata.page.site.selector')->retrieve();
+            } catch (\RuntimeException $e) {
+                $current = false;
+            }
+
+            return $this->render('SonataPageBundle:PageAdmin:select_site.html.twig', array(
+                'sites'   => $sites,
+                'current' => $current,
+            ));
+        }
+
+        return parent::createAction();
+    }
     /**
      * return the Response object associated to the edit action
      *
