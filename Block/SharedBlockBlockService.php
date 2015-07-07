29d4c6c944cd994491d1a2305f90946581a9ae39
mellzamora <mellzamora@outlook.com>
2015-07-07 10:35:42 +0800

    fixed shared block error

--- Block/SharedBlockBlockService.php
+++ Block/SharedBlockBlockService.php
@@ -128,7 +128,8 @@ class SharedBlockBlockService extends BaseBlockService
     public function setDefaultSettings(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'template' => 'RzPageBundle:Block:block_shared_block.html.twig'
+            'template' => 'RzPageBundle:Block:block_shared_block.html.twig',
+            'blockId' => null
         ));
     }
 
--- Controller/BlockAdminController.php
+++ Controller/BlockAdminController.php
@@ -169,74 +169,74 @@ class BlockAdminController extends Controller
      * @param Request $request
      * @return Response
      */
-    public function editAction($id = null, Request $request = null)
-    {
-        // the key used to lookup the template
-        $templateKey = 'edit';
-
-        $id = $request->get($this->admin->getIdParameter());
-
-        $object = $this->admin->getObject($id);
-
-        if (!$object) {
-            throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
-        }
-
-        if (false === $this->admin->isGranted('EDIT', $object)) {
-            throw new AccessDeniedException();
-        }
-
-        $this->admin->setSubject($object);
-
-        /** @var $form \Symfony\Component\Form\Form */
-        $form = $this->admin->getForm();
-        $form->setData($object);
-
-        if ($this->getRestMethod() == 'POST') {
-
-            $form->handleRequest($request);
-
-            $isFormValid = $form->isValid();
-
-            // persist if the form was valid and if in preview mode the preview was approved
-            if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
-                $this->admin->update($object);
-
-                if ($this->isXmlHttpRequest()) {
-                    return $this->renderJson(array(
-                        'result'    => 'ok',
-                        'objectId'  => $this->admin->getNormalizedIdentifier($object)
-                    ));
-                } else {
-                    $this->addFlash('sonata_flash_success', 'flash_edit_success');
-                }
-
-                // redirect to edit mode
-                return $this->redirectTo($object);
-            }
-
-            // show an error message if the form failed validation
-            if (!$isFormValid) {
-                if (!$this->isXmlHttpRequest()) {
-                    $this->addFlash('sonata_flash_error', 'flash_edit_error');
-                }
-            } elseif ($this->isPreviewRequested()) {
-                // enable the preview template if the form was valid and preview was requested
-                $templateKey = 'preview';
-            }
-        }
-
-        $view = $form->createView();
-
-        // set the theme for the current Admin Form
-        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
-
-        return $this->render($this->admin->getTemplate($templateKey), array(
-            'action' => 'edit',
-            'form'   => $view,
-            'object' => $object,
-        ));
-    }
+//    public function editAction($id = null, Request $request = null)
+//    {
+//        // the key used to lookup the template
+//        $templateKey = 'edit';
+//
+//        $id = $request->get($this->admin->getIdParameter());
+//
+//        $object = $this->admin->getObject($id);
+//
+//        if (!$object) {
+//            throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
+//        }
+//
+//        if (false === $this->admin->isGranted('EDIT', $object)) {
+//            throw new AccessDeniedException();
+//        }
+//
+//        $this->admin->setSubject($object);
+//
+//        /** @var $form \Symfony\Component\Form\Form */
+//        $form = $this->admin->getForm();
+//        $form->setData($object);
+//
+//        if ($this->getRestMethod() == 'POST') {
+//
+//            $form->handleRequest($request);
+//
+//            $isFormValid = $form->isValid();
+//
+//            // persist if the form was valid and if in preview mode the preview was approved
+//            if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
+//                $this->admin->update($object);
+//
+//                if ($this->isXmlHttpRequest()) {
+//                    return $this->renderJson(array(
+//                        'result'    => 'ok',
+//                        'objectId'  => $this->admin->getNormalizedIdentifier($object)
+//                    ));
+//                } else {
+//                    $this->addFlash('sonata_flash_success', 'flash_edit_success');
+//                }
+//
+//                // redirect to edit mode
+//                return $this->redirectTo($object);
+//            }
+//
+//            // show an error message if the form failed validation
+//            if (!$isFormValid) {
+//                if (!$this->isXmlHttpRequest()) {
+//                    $this->addFlash('sonata_flash_error', 'flash_edit_error');
+//                }
+//            } elseif ($this->isPreviewRequested()) {
+//                // enable the preview template if the form was valid and preview was requested
+//                $templateKey = 'preview';
+//            }
+//        }
+//
+//        $view = $form->createView();
+//
+//        // set the theme for the current Admin Form
+//        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+//
+//        return $this->render($this->admin->getTemplate($templateKey), array(
+//            'action' => 'edit',
+//            'form'   => $view,
+//            'object' => $object,
+//        ));
+//    }
 
     public function switchParentAction(Request $request = null)
     {
